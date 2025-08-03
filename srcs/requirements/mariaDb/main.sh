d359cb9e98542718791b15c0e60bb1fd91ea6725
Lucabhm <lbohm@student.42heilbronn.de>
2025-08-04 00:58:30 +0200

    fixed installation mariadb and wordpress

--- srcs/requirements/mariaDb/main.sh
+++ srcs/requirements/mariaDb/main.sh
@@ -4,18 +4,20 @@ mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
 if [ ! -d /var/lib/mysql/mysql ]; then
-	mariadb-install-db --user=lbohm --basedir=/usr --datadir=/var/lib/mysql
+	mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 fi
+
+mysqld --user=mysql --datadir=/var/lib/mysql & pid="$!"
 
 while ! /usr/bin/mariadb-admin ping --silent; do sleep 1; done
 
 mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'securepass';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
+CREATE DATABASE IF NOT EXISTS wp_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'wp_pass';
+GRANT ALL PRIVILEGES ON wp_db.* TO 'wp_user'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-wait
+wait "$pid"
--- srcs/requirements/wordPress/wordpress.sh
+++ srcs/requirements/wordPress/wordpress.sh
@@ -3,23 +3,30 @@
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 rm latest.tar.gz
+mv wordpress/* /var/www/html
+rm -rf wordpress
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+cd /var/www/html
+
 if [ ! -f "wp-config.php" ]; then
 	wp config create \
-		--dbname=your_database_name \
-		--dbuser=your_database_user \
-		--dbpass=your_database_password \
-		--dbhost=your_database_host
+		--dbname=wp_db \
+		--dbuser=wp_user \
+		--dbpass=wp_pass \
+		--dbhost=mariadb
 
 	wp core install \
-		--url=example.com \
+		--url=https://localhost \
 		--title=Example \
 		--admin_user=supervisor \
-		--admin_password="test"
+		--admin_password="test" \
+		--admin_email=admin@example.com
 fi
+
+chown -R www-data:www-data /var/www/html
 
 php-fpm82 -F
