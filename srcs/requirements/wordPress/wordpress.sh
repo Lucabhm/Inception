85b422035559e1631aa0a742a905c123f0603302
Lucabhm <lbohm@student.42heilbronn.de>
2025-08-04 17:34:29 +0200

    try to implemt it with default config maridb and wordpress

--- srcs/requirements/wordPress/wordpress.sh
+++ srcs/requirements/wordPress/wordpress.sh
@@ -5,30 +5,42 @@ set -x
 
 cd /var/www/html
 
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz --strip-components=1
-rm latest.tar.gz
-
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=lbohm --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
-if [ ! -f "wp-config.php" ]; then
-	wp config create \
-		--dbname=wp_db \
-		--dbuser=wp_user \
-		--dbpass=wp_pass \
-		--dbhost=mariadb:3306 \
-		--allow-root
 
-	wp core install \
-		--url=https://localhost \
-		--title=Example \
-		--admin_user=supervisor \
-		--admin_password="test" \
-		--admin_email=admin@example.com
-fi
+# wget https://wordpress.org/latest.tar.gz
+# tar -xzf latest.tar.gz --strip-components=1
+# rm latest.tar.gz
 
-chown -R www-data:www-data /var/www/html
+# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp
+
+# if [ ! -f "wp-config.php" ]; then
+
+# 	wp core download \
+# 		--allow-root
+
+# 	wp config create \
+# 		--dbname=wordpress \
+# 		--dbuser=lbohm \
+# 		--dbpass=password \
+# 		--dbhost=mariadb \
+# 		--allow-root
+
+# 	wp core install \
+# 		--url=localhost \
+# 		--title=inception \
+# 		--admin_user=admin \
+# 		--admin_password=admin \
+# 		--admin_email=admin@admin.com \
+# 		--allow-root
+# fi
+
+# chown -R www-data:www-data /var/www/html
 
 php-fpm8.2 -F
