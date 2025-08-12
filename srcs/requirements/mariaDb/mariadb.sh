49da1426189728022af51bddd8c891d3689ad1db
Lucabhm <lbohm@student.42heilbronn.de>
2025-08-12 10:19:38 +0200

    add in mariadb.sh change owner path

--- srcs/requirements/mariaDb/mariadb.sh
+++ srcs/requirements/mariaDb/mariadb.sh
@@ -1,24 +1,26 @@
 #! /bin/sh
 
 mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -d /var/lib/mysql/mysql ]; then
 	mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+	mysqld_safe --bind-address=0.0.0.0 &
+	sleep 10
+
+	until mysqladmin ping --silent; do
+		sleep 2; 
+	done
+
+	mariadb -u root <<-EOF
+	ALTER USER 'root'@'localhost' IDENTIFIED BY "${MYSQL_ROOT_PASSWORD}";
+
+	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY "${MYSQL_PASSWORD}";
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO "${MYSQL_USER}"@'%';
+	FLUSH PRIVILEGES;
+	EOF
 fi
-
-mysqld_safe --bind-address=0.0.0.0 &
-sleep 10
-
-while ! mysqladmin ping --silent; do sleep 2; done
-
-mariadb -u root <<-EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY "${MYSQL_ROOT_PASSWORD}";
-
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY "${MYSQL_PASSWORD}";
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO "${MYSQL_USER}"@'%';
-FLUSH PRIVILEGES;
-EOF
 
 wait
