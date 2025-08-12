c956188aea49b9853d072dbe0bb0709a31093e29
Lucabhm <lbohm@student.42heilbronn.de>
2025-08-12 11:28:36 +0200

    change mariadb.sh

--- srcs/requirements/mariaDb/mariadb.sh
+++ srcs/requirements/mariaDb/mariadb.sh
@@ -4,23 +4,22 @@ mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -d /var/lib/mysql/mysql ]; then
-	mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
-	mysqld_safe --bind-address=0.0.0.0 &
-	sleep 10
-
-	until mysqladmin ping --silent; do
-		sleep 2; 
-	done
-
-	mariadb -u root <<-EOF
-	ALTER USER 'root'@'localhost' IDENTIFIED BY "${MYSQL_ROOT_PASSWORD}";
-
-	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY "${MYSQL_PASSWORD}";
-	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO "${MYSQL_USER}"@'%';
-	FLUSH PRIVILEGES;
-	EOF
+	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null 2>&1
 fi
+
+mysqld_safe --datadir=/var/lib/mysql --bind-address=0.0.0.0 &
+sleep 10
+
+until mysqladmin ping --silent; do
+	sleep 2; 
+done
+
+mysql -u root <<EOF
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+	CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
+	CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+	GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
+	FLUSH PRIVILEGES;
+EOF
 
 wait
